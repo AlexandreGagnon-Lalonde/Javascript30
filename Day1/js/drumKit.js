91d39a415619307c44dda56839920686ec7a1833
TheMadPig <49966112+TheMadPig@users.noreply.github.com>
2020-03-13 19:16:27 -0400

    Working prototype

---
+++ Day1/js/drumKit.js
@@ -0,0 +1,16 @@
+document.addEventListener("keydown", playAudio);
+
+function playAudio(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    console.log(e.keyCode);
+    if (!audio) {
+        return
+    }
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playing");
+    setTimeout(function(){
+        key.classList.remove("playing")
+    }, 70);
+}
